f855af32f564c13ba2bb2c25ea007ccbbc627b39
Mark Wilbrink <98471282+wilbrinkdesign@users.noreply.github.com>
2023-01-22 22:13:03 +0100

    Delete Parameter PowerShell.ps1

--- Parameter PowerShell.ps1
+++
@@ -1,3 +0,0 @@
-# Parameter meegeven
-$manager = @{ Manager = "<manager_naam>" } # New-ADUser <username> @manager => New-ADUser <username> -Manager "<manager_naam>"
-$manager = @{ Manager = $True } # New-ADUser <username> @manager => New-ADUser <username> -Manager
