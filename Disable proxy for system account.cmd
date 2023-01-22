ab3ac7ece971db3d3e5e14ddcd755435174c4b86
Mark Wilbrink <98471282+wilbrinkdesign@users.noreply.github.com>
2023-01-22 21:43:59 +0100

    Update and rename Disable proxy for system account.cmd to Disable proxy for system account.md

--- Disable proxy for system account.cmd
+++
@@ -1,3 +0,0 @@
-bitsadmin /Util /SetIEProxy localsystem AUTODETECT
-bitsadmin /util /setieproxy localsystem MANUAL_PROXY proxy1,proxy2,proxy3 NULL
-bitsadmin /util /setieproxy localsystem MANUAL_PROXY proxy1:80 ""
