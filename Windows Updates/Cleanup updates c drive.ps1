818b7d21002f8c64268e782129dd45bb76d2ba9a
Mark Wilbrink <98471282+wilbrinkdesign@users.noreply.github.com>
2023-01-22 21:26:03 +0100

    Update and rename Cleanup updates c drive.ps1 to Cleanup updates.md

--- Windows Updates/Cleanup updates c drive.ps1
+++
@@ -1,5 +0,0 @@
-# Clean updates die langer dan 30 dagen op het systeem staan
-dism /online /Cleanup-Image /StartComponentCleanup
-
-# Clean alle updates
-dism /online /Cleanup-Image /StartComponentCleanup /ResetBase
