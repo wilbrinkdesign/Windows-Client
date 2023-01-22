9c2e6d21192e3458c1aa118aaef5a437a0ee9576
Mark Wilbrink <98471282+wilbrinkdesign@users.noreply.github.com>
2023-01-22 21:42:20 +0100

    Delete Delete credentials.cmd

--- Delete credentials.cmd
+++
@@ -1,2 +0,0 @@
-ECHO Specifieke credentials verwijderen uit de referentie store
-for /F "tokens=1,2,3 delims==" %G in ('cmdkey /list ^| findstr "teams OneDrive Office outlook"') do cmdkey /delete:%H
