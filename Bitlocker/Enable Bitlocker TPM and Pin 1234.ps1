39b8ad452e928e8f6c9a71bba7c7892620c51c8f
Mark Wilbrink <98471282+wilbrinkdesign@users.noreply.github.com>
2023-01-22 21:23:24 +0100

    Update and rename Bitlocker TPM and Pin.ps1 to Enable Bitlocker TPM and Pin 1234.ps1

--- Bitlocker/Enable Bitlocker TPM and Pin 1234.ps1
+++ Bitlocker/Enable Bitlocker TPM and Pin 1234.ps1
@@ -1,14 +1,8 @@
-Write-Host "Status opvragen voordat we beginnen met verwijderen..."
-manage-bde -status C:
-
 manage-bde -protectors -delete "C:" -type tpm
 manage-bde -protectors -delete "C:" -type tpmandpin
 manage-bde -protectors -delete "C:" -type password
 manage-bde -protectors -delete "C:" -type recoverypassword
 manage-bde -off C:
-
-Write-Host "Status opvragen nadat we alle protectors hebben verwijderd..."
-manage-bde -status C:
 
 Do
 {
@@ -17,12 +11,6 @@ Do
     $VolumeStatus = (Get-BitLockerVolume C:).VolumeStatus
 } While ($VolumeStatus -eq "DecryptionInProgress")
 
-Write-Host "Bitlocker inschakelen..."
 Enable-BitLocker -MountPoint "C:" -EncryptionMethod XtsAes256 -RecoveryPasswordProtector -UsedSpaceOnly
-
-Write-Host "Bitlocker TPM + PIN instellen..."
 $pin = ConvertTo-SecureString "1234" -AsPlainText -Force
 Add-BitlockerKeyProtector -Mountpoint "C:" -Pin $pin -TpmAndPinProtector
-
-Write-Host "Status opvragen nadat we klaar zijn..."
-manage-bde -status C:
